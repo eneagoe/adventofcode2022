fec420789b06fab9286c2e17d7d55db50e30fa2e
eugen neagoe <eneagoe@gmail.com>
2022-12-02 19:21:05 +0200

    Add solution for 2nd puzzle of 2nd day

--- day2.rb
+++ day2.rb
@@ -17,3 +17,21 @@ total_score = File.open(ARGV[0]).readlines.map do |line|
 end.reduce(:+)
 
 puts total_score
+
+wins = {"rock" => 2, "paper" => 3, "scissors" => 1}
+loses = {"rock" => 3, "paper" => 1, "scissors" => 2}
+
+# 2nd puzzle
+strategic_score = File.open(ARGV[0]).readlines.map do |line|
+  a, b = line.split(/\s+/)
+
+  if b == "X"
+    loses[opp[a]]
+  elsif b == "Y"
+    scores[opp[a]] + 3
+  else
+    wins[opp[a]] + 6
+  end
+end.reduce(:+)
+
+puts strategic_score
