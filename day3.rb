e0f37ca9b85e7a1f59eb48cd99af0558ce261920
eugen neagoe <eneagoe@gmail.com>
2022-12-03 21:04:33 +0200

    Add solution for 2nd puzzle of the 3rd day

--- day3.rb
+++ day3.rb
@@ -4,6 +4,7 @@ require 'set'
 
 priorities = Hash[('A'..'Z').zip(27..52)].merge Hash[('a'..'z').zip(1..26)]
 
+# 1st puzzle
 priorities_sum = File.open(ARGV[0]).readlines.sum do |line|
   first, second = line.chars.each_slice(line.size / 2).map { Set.new _1 }
 
@@ -11,3 +12,10 @@ priorities_sum = File.open(ARGV[0]).readlines.sum do |line|
 end
 
 puts priorities_sum
+
+# 2nd puzzle
+badge_priorities = File.open(ARGV[0]).readlines.each_slice(3).map do |a, b, c|
+  (Set.new(a.chomp.chars) & Set.new(b.chomp.chars) & Set.new(c.chomp.chars)).sum { priorities[_1] }
+end.sum
+
+puts badge_priorities
