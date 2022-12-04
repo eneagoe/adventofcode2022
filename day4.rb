5d8192a1719418796dd9d5a8416e727136464e98
eugen neagoe <eneagoe@gmail.com>
2022-12-04 21:20:32 +0200

    Add solution for 2nd puzzle of 4th day

--- day4.rb
+++ day4.rb
@@ -8,3 +8,12 @@ inc = File.open(ARGV[0]).readlines.count do |line|
 end
 
 puts inc
+
+# 2nd puzzle
+overlaps =  File.open(ARGV[0]).readlines.count do |line|
+  a, b = line.chomp.split(",").map { _1.split("-").map(&:to_i) }
+
+  a[0] <= b[0] && b[0] <= a[1] || b[0] <= a[0] && a[0] <= b[1]
+end
+
+puts overlaps
