0b19b806b0d3ed008f89bdcceaf33c60f344e407
eugen neagoe <eneagoe@gmail.com>
2022-12-04 21:13:53 +0200

    Add solution for 1st puzzle of the 4th day

---
+++ day4.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby -w
+
+# 1st puzzle
+inc = File.open(ARGV[0]).readlines.count do |line|
+  a, b = line.chomp.split(",").map { _1.split("-").map(&:to_i) }
+
+  a[0] <= b[0] && b[1] <= a[1] || b[0] <= a[0] && a[1] <= b[1]
+end
+
+puts inc
