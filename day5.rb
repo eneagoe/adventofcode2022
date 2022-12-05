5df9335dec380e0639408319eee344ba39431a3f
eugen neagoe <eneagoe@gmail.com>
2022-12-05 19:52:04 +0200

    Add solution for 2nd puzzle of 5th day

--- day5.rb
+++ day5.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby -w
 
 stacks = []
+stacks2 = []
 
 # 1st puzzle
 File.open(ARGV[0]).readlines.count do |line|
@@ -16,14 +17,22 @@ File.open(ARGV[0]).readlines.count do |line|
       $1.to_i.times do
         stacks[to].insert(0, stacks[from].shift(1))
       end
+
+      moving = stacks2[from].shift($1.to_i)
+      stacks2[to].unshift(*moving)
     end
   else
     # initialize stacks
     line.chomp.chars.each_slice(4).with_index do |s, idx|
       stacks.append([]) if stacks.size <= idx
+      stacks2.append([]) if stacks2.size <= idx
+
       stacks[idx].append(s[1]) unless s[1] == " "
+      stacks2[idx].append(s[1]) unless s[1] == " "
     end
   end
 end
 
 puts stacks.map(&:first).join("")
+# 2nd puzzle
+puts stacks2.map(&:first).join("")
