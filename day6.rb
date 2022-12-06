e9ca4dc5af830848eae3a643cc15a15cc1434d8f
eugen neagoe <eneagoe@gmail.com>
2022-12-06 17:11:35 +0200

    Add solution for 2nd puzzle of the 6th day

--- day6.rb
+++ day6.rb
@@ -2,11 +2,20 @@
 
 require 'set'
 
-# 1st puzzle
 File.open(ARGV[0]).readlines.each do |line|
+  # 1st puzzle
   line.chomp.chars.each_cons(4).with_index do |chars, i|
     if Set.new(chars).size == 4
       puts i + 4
+      break
+    end
+  end
+
+  # 2nd puzzle
+  line.chomp.chars.each_cons(14).with_index do |chars, i|
+    if Set.new(chars).size == 14
+      puts i + 14
+      puts
       break
     end
   end
